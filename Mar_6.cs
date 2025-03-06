0c59a36db929fcbe4a6fc52dbbed6dd2cc9c4840
sahanayuvaraj <SahanaY857@stemcollegiate.ca>
2025-03-06 10:29:40 -0700

    Create Mar_6.cs

---
+++ Mar_6.cs
@@ -0,0 +1,7 @@
+//HAPPY Ghana Birthday
+//We learned how to use the string thing and we also used the bool thing for true and false value
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
