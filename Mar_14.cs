b3450bd9403c1306931865896827b2205bdc941f
sahanayuvaraj <SahanaY857@stemcollegiate.ca>
2025-03-14 08:52:04 -0600

    Create Mar_14.cs

---
+++ Mar_14.cs
@@ -0,0 +1,8 @@
+//Module 4 UNIT 3 & 4 
+string firstName = "sahana";
+string greeting = "Hello";
+Console.WriteLine(greeting + " " + firstName + "!");
+string projectName = "sahana's-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+// you can combine variables using the + sign and the $ and @ reduces unnecesary codes.
