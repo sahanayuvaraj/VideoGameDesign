d48b1d53df9e1007d557e15a11956b2a679c8903
sahanayuvaraj <SahanaY857@stemcollegiate.ca>
2025-04-10 10:10:05 -0600

    Create Apr_10.cs

---
+++ Apr_10.cs
@@ -0,0 +1,5 @@
+//Module 5 Unit 1 & 2
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+//The + sign can add numbers or join words, and you can use brackets to show what should happen first.
